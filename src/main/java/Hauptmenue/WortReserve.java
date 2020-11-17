665c76ff8429cd1030efea05e87364bf64e66ad6
MatisGourdes <73692139+MatisGourdes@users.noreply.github.com>
2020-11-17 21:49:31 +0100

    Start Wort Programm

---
+++ src/main/java/Hauptmenue/WortReserve.java
@@ -0,0 +1,12 @@
+package Hauptmenue;
+
+public class WortReserve {
+
+   public static String CitizenWort[]={ "tomate","Paris","Arbre","fleur","Mur de Berlin"};
+
+
+
+    public static String UndercoverWort[]= {"tomate cerise","New York","buche","rose","Muraille de Chine"};
+
+
+}
