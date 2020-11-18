de2e423bedbd87f90389b036670ef50c636e5c3d
Maxime Roellinger <maxime.roellinger1@gmail.com>
2020-11-18 13:57:01 +0100

    first comit

--- src/main/java/Hauptmenue/ControllerWortAusgabe.java
+++ src/main/java/Hauptmenue/ControllerWortAusgabe.java
@@ -17,37 +17,44 @@ public class ControllerWortAusgabe extends Controller {
     @FXML
     private Label WortAusgabe;
 
+    @FXML
+      private Label BefehleWortAusgabe;
+
     String printLabelWort;
 
     int WortRandom= (int)(Math.random()*5);
 
-
+int i=0;
 public void SwitchToNextPLayer(ActionEvent event) throws IOException {
 
-        for (int i = 0; i < spielerZahl; i++) {
-            PrintWord(i); // demander je veux mtn que ca continue quand j'appuie sur le bouton michael aide moi wsh
-           WortAusgabe.setText(" ");
+
+
+
+             switch(Person.RolleArray[i]){// Ici changer le 1
+                 case 0:
+                     printLabelWort = WortReserve.CitizenWort[WortRandom];
+
+                     break;
+                 case 1:
+                     printLabelWort = WortReserve.UndercoverWort[WortRandom];
+
+
+                     break;
+                 case 2 :
+
+
+                     break;
+             }
+           WortAusgabe.setText(" voici ton mot "+printLabelWort);
+    BefehleWortAusgabe.setText("salam "+i);
+             i++;
         }
+
+
+
+
+
+
+
     }
 
-
-
-
-    public void PrintWord(int i){
-        switch(Person.RolleArray[i]){// Ici changer le 1
-            case 0:
-            printLabelWort = WortReserve.CitizenWort[WortRandom];
-                WortAusgabe.setText(printLabelWort);
-                break;
-            case 1:
-                printLabelWort = WortReserve.UndercoverWort[WortRandom];
-                WortAusgabe.setText(printLabelWort);
-
-                break;
-            case 2 :
-
-
-                break;
-    }
-
-}}
