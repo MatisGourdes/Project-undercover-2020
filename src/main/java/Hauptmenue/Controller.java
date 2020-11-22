d55c18705d1ea525986afdda9dabf3b68b2f14f7
MatisGourdes <73692139+MatisGourdes@users.noreply.github.com>
2020-11-22 15:11:40 +0100

    Merging Master and Mikaelle's Work

--- src/main/java/Hauptmenue/Controller.java
+++ src/main/java/Hauptmenue/Controller.java
@@ -1,5 +1,7 @@
 package Hauptmenue;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,13 +9,36 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Vector;
 
 public class Controller {
+    @FXML
+    private Button addPlayerBtn = new Button();
 
-    int SpielerZahl = 8; //Programm ist bis Sprint 2 immer mit 8 Spieler, Hier in der Variabel wechseln
+    @FXML
+    private TextField eingabeName;
+    @FXML
+    private Label spielerNrLabel;
+    @FXML
+    private TextField eingabeAnzahlSpieler;
+    @FXML
+    private Button speichern = new Button();
+    @FXML
+    private TableView<Spieler> tableViewSpieler;
+    @FXML
+    private TableColumn<Spieler, Integer> nrTableView;
+    @FXML
+    private TableColumn<Spieler, String> nameTableView;
+
+    private double anzahlSpieler;
+    private static int spielerNr = 1;
+
+    //Liste aller Spieler
+    static Vector<Spieler> spielerListe = new Vector<Spieler>();
 
     String printLabelWort;
 
@@ -40,6 +65,10 @@ public class Controller {
     private Button btnWorter;
 
 
+    public void initialize(){
+        addPlayerBtn.setDisable(true);
+
+    }
 
     @FXML
     // Wechseln zu Spielregeln View
@@ -48,21 +77,6 @@ public class Controller {
         Stage window = new Stage();
         window.setScene(new Scene(spielRegelnScene));
         window.setTitle("Spielregeln");
-        window.show();
-    }
-
-
-    @FXML
-    public void startGame(ActionEvent event) throws IOException {
-        Person.DefineRolle(SpielerZahl);// Programm definiert eine Random rolle für die Spieler von 0 bis 7
-        Person.DefineAllAlive(SpielerZahl);// Dieser programm definiert das alle Spieler am Leben sind
-        Person.DefineRolleZuSpieler(SpielerZahl);
-        Person.ShowMyWork(SpielerZahl);// Ce programme est provisoire et donne les informations que les joueurs ne veront pas
-        Parent WortAusgabeParent = FXMLLoader.load(getClass().getResource("WortAusgabe.fxml"));// Hier werden die Spieler Namen gefragt
-        Scene WortAusgabeScene = new Scene(WortAusgabeParent);
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(WortAusgabeScene);
-        window.setTitle("WortAusgabe");
         window.show();
     }
 
@@ -77,7 +91,74 @@ public class Controller {
         MindmapStage.show();
     }
 
+    // Wechseln zu Spiel View
+    public void switchToSpiel(ActionEvent event) throws IOException {
+        Parent spielParent = FXMLLoader.load(getClass().getResource("addSpieler.fxml"));
+        Scene spielScene = new Scene(spielParent);
+        //get stage info
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(spielScene);
+        window.setTitle("Init");
+        window.show();
+    }
 
+    // Anzahl Spieler eingeben
+    public void save(ActionEvent event) throws IOException{
+        anzahlSpieler = Integer.parseInt(eingabeAnzahlSpieler.getText());
+        eingabeAnzahlSpieler.setDisable(true);
+        speichern.setText("gespeichert !");
+        speichern.setDisable(true);
+        spielerNrLabel.setText("Spieler 1:");
+        addPlayerBtn.setDisable(false);
+        //initialise colonnes tableviewSpieler
+        nrTableView.setCellValueFactory(new PropertyValueFactory<Spieler, Integer>("spielerNr"));
+        nameTableView.setCellValueFactory(new PropertyValueFactory<Spieler, String>("name"));
+    }
+
+
+
+    // Spieler hinzufügen
+    public void addPlayer(ActionEvent event) throws IOException {
+        Spieler temp = new Spieler(spielerNr, eingabeName.getText(), true, 0, new Button());
+        spielerListe.add(temp);
+
+        if (spielerNr < anzahlSpieler) {
+            eingabeName.clear();
+            spielerNrLabel.setText("Spieler " + String.valueOf(spielerNr + 1) + ":");
+
+        }
+        else {
+            addPlayerBtn.setDisable(true);
+            eingabeName.setDisable(true);
+            spielerNrLabel.setText("Tip top");
+
+            // RolleZuweisung.randomRolle();
+        }
+        spielerNr++;
+
+        //debug
+        for (int i = 0; i < spielerListe.size(); i++) {
+            System.out.println(spielerListe.elementAt(i).getSpielerNr() + ": " +
+                    spielerListe.elementAt(i).getName() + " " + Spieler.rolleName(spielerListe.elementAt(i).getRolle()));
+        }
+        System.out.println("___");
+
+        //display in der Tabelle
+        tableViewSpieler.setItems(getPeople());
+    }
+
+    //display in der Tabelle
+    public ObservableList<Spieler> getPeople(){
+        ObservableList<Spieler> list = FXCollections.observableArrayList();
+        for (int i = 0; i < spielerListe.size(); i++) {
+            list.add(spielerListe.elementAt(i));
+        }
+        return list;
+
+    }
+
+    //RENAME INTO SWITCHNEXTPLAYER TO GIVE WORD
+    //Programm to give the word
     public void SwitchToNextPLayer(ActionEvent event) throws IOException {
 
         btnWorter.setText("Nächste Spieler");
@@ -85,7 +166,7 @@ public class Controller {
         HideWord.setText("click to show");
         swich = false;
 
-        switch (Person.RolleArray[i]) {// Ici changer le 1
+        switch (spielerListe.elementAt(i).getRolle()) {// Ici changer le 1
             case 0:
                 printLabelWort = WortReserve.CitizenWort[WortRandom];
 
@@ -97,23 +178,19 @@ public class Controller {
                 printLabelWort = "Du bist Mr White, versuch dich nicht auffallen lassen  ";
                 break;
         }
-        BefehleWortAusgabe.setText("Hallo " + Person.Spieler[i]);
+        BefehleWortAusgabe.setText("Hallo " + spielerListe.elementAt(i).getName());
         if (7 >= i && declic == 1) {
             if (i == 7) {
                 i = 7;
                 declic = 0;}
-             else {i++;}
+            else {i++;}
 
 
         } else {befehlWindow(event);}
-
-
     }
 
 
-
-
-
+    //Method um den Wort zu zeigen oder nicht wen er gegeben ist
     public void swichToShow(ActionEvent event) throws IOException {
 
         if (i > 0) {
@@ -141,5 +218,12 @@ public class Controller {
 
     }
 
+
+        public static Vector<Spieler> getSpielerListe() {
+            return spielerListe;
+        }
+        public static void setSpielerListe(Vector<Spieler> spielerListe) {
+            Controller.spielerListe = spielerListe;
+        }
 
 }
---
+++ src/main/java/Hauptmenue/Spieler.java
@@ -0,0 +1,65 @@
+package Hauptmenue;
+
+import javafx.scene.control.Button;
+
+public class Spieler {
+
+    private String name;
+    public static Button btn;
+    private boolean status;
+    private int rolle, spielerNr;
+
+    public Spieler(int spielerNr, String name, boolean status, int rolle, Button btn){
+        this.spielerNr = spielerNr;
+        this.name = name;
+        this.status = status;
+        this.rolle = rolle;
+        this.btn = btn;
+        this.btn.setText(name);
+        this.btn.setId(name);
+
+        rolleName(this.rolle);
+    }
+
+public static String rolleName(int rolle){
+        String Rolle ="";
+        switch (rolle){
+            case 0:
+                Rolle = "Citizen";
+                break;
+            case 1:
+                Rolle = "Undercover";
+                break;
+            case 2:
+                Rolle = "Mr White";
+                break;
+        }
+        return Rolle;
+}
+
+    public int getSpielerNr() { return spielerNr; }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getRolle() {
+        return rolle;
+    }
+
+    public void setRolle(int rolle) {
+        this.rolle = rolle;
+    }
+}
