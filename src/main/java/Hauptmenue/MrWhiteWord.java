84294c189d5f9a6d29f74d5a910a2c29b7fb2e37
Theo Portmann <pro.portmanntheo68@gmail.com>
2020-11-24 18:25:48 +0100

    .

--- src/main/java/Hauptmenue/MrWhiteWord.java
+++ src/main/java/Hauptmenue/MrWhiteWord.java
@@ -22,9 +22,6 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 
-import static Hauptmenue.Person.Spieler;
-import static Hauptmenue.WortReserve.CitizenWort;
-import static javafx.application.Application.launch;
 
 
 public class MrWhiteWord extends Application {
@@ -33,13 +30,13 @@ public class MrWhiteWord extends Application {
         launch(args);
 
     }
-
     @FXML
     private TextField Input;
 
+
 String wort;
 
-    int WortRandom= (int)(Math.random()*5);
+
     @Override
     public void start(final Stage primaryStage) throws IOException {
 
@@ -52,22 +49,11 @@ String wort;
         primaryStage.show();
     }
 
-    @FXML
-    public void test(ActionEvent event) throws IOException {
-
-        wort=Input.getText();
-System.out.println(WortReserve.CitizenWort[WortRandom]);
-        if(wort.equalsIgnoreCase(WortReserve.CitizenWort[WortRandom])){
-            System.out.println("Korrektes Wort");}
-        else{
-            System.out.println("Falsches Wort Citizen haben gewonnen");
-
-}
 
 
     }
 
-}
+
 
 
 
