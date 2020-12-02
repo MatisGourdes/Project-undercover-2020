453f1128d932dbb5b36243a2867b870a973f46a9
Schaetzel Michael <mica.schaetzel@gmail.com>
2020-12-02 18:58:45 +0100

    50% tested in domain

---
+++ src/test/java/domain/WortReserveTest.java
@@ -0,0 +1,15 @@
+package domain;
+
+import junit.framework.TestCase;
+
+public class WortReserveTest extends TestCase {
+    private String wortCitizen, wortUndercover;
+    protected void setUp() throws Exception{
+        wortCitizen = "testCitizen";
+        wortUndercover = "testUndercover";
+    }
+
+    public void testEingabeWoerter() throws Exception{
+
+    }
+}
