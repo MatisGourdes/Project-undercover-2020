a5798dfa4f3de36fe8087006c48ccec8527db36a
Maxime Roellinger <maxime.roellinger1@gmail.com>
2020-12-07 15:21:49 +0100

    add relation avec "Person.ungeordneteplayer" dans ControllerWortAusgabe

--- src/test/java/presentation/WortAusgabeControllerTest.java
+++ src/test/java/presentation/WortAusgabeControllerTest.java
@@ -1,4 +1,24 @@
 package presentation;
 
+import domain.Spieler;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class WortAusgabeControllerTest {
+@Test
+  public void createNewSpieler()
+
+  {
+
+
+  Spieler spieler1 = new Spieler(1, "nameA", true, 0);
+  Spieler spieler2 = new Spieler(2, "nameB", true, 1);
+  Spieler spieler3 = new Spieler(3, "nameC", true, 2);
+
+    Assertions.assertEquals(0,spieler1.getRolle());
+    Assertions.assertEquals(1,spieler2.getRolle());
+    Assertions.assertEquals(2,spieler3.getRolle());
+}
+
+
 }
