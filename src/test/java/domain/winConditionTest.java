5f2c4483e347f1dc439081c7c15a6e49a15db36a
Theo Portmann <pro.portmanntheo68@gmail.com>
2020-12-14 14:17:59 +0100

    Tests

--- src/test/java/domain/winConditionTest.java
+++ src/test/java/domain/winConditionTest.java
@@ -1,67 +1,78 @@
 package domain;
 
 import junit.framework.TestCase;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import presentation.Controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class winConditionTest extends TestCase {
-
-
-    Spieler testSpieler = new Spieler(1, "TestS", true, 2);
-        @Test
-        public void testUndercover() throws Exception {
-
-            boolean boolCitizen = false;
-            double summeRollen = 0;
-            int SpielerAmLeben=0;
-            Controller.getSpielerListe().setSize(1);
-
-
-            for(int i = 0; i< Controller.getSpielerListe().size(); i++) {
-
-                if(testSpieler.getStatus()==true){
-                    SpielerAmLeben++;
-                    if(testSpieler.getRolle()==1){
-                        summeRollen += 1 ;
-                    }}
-            }
-            summeRollen = summeRollen/ SpielerAmLeben;
-            if(summeRollen == 1)
-            {boolCitizen = true;}
-
-            else{boolCitizen = false; }
+public class winConditionTest extends TestCase {
 
 
 
 
+@Test
+    public void TtestUndercover() throws Exception {
 
 
-            assertFalse(boolCitizen);
-        }
+    boolean boolCitizen = false;
+    double summeRollen = 1;
+    int SpielerAmLeben = 1;
 
-        public void TestCitizen() throws Exception{
 
-            boolean boolCitizen = false;
-            int summeRollen = 0;
-            Controller.getSpielerListe().setSize(1);
-            for(int i = 0; i< Controller.getSpielerListe().size(); i++) {
-                if(testSpieler.getStatus()==true){
-                    summeRollen += Controller.getSpielerListe().elementAt(i).getRolle();
-                }}
-            if(summeRollen == 0){
-                boolCitizen = true;
+    for(int i = 0; i< Controller.getSpielerListe().size(); i++) { }
+    summeRollen = summeRollen/ SpielerAmLeben;
+    if(summeRollen == 1)
+    {boolCitizen = true;}
 
-            }
-            else {
-                boolCitizen = false;
+    else{boolCitizen = false; }
 
-            }
-            assertFalse(boolCitizen);
+
+
+
+    assertTrue(boolCitizen);
+}
+
+    @Test
+    void Ttest2Spieler() {
+        boolean boolCitizen = false;
+        int spielerAmLeben = 0;
+        for(int i = 0; i< Controller.getSpielerListe().size(); i++) {
+            if(Controller.getSpielerListe().elementAt(i).getStatus()==true){
+                spielerAmLeben ++;
+            }}
+        if( spielerAmLeben == 2){
+            boolCitizen = true;
 
         }
+        else {
+            boolCitizen = false;
 
+        }
+        assertTrue(boolCitizen);
     }
 
 
+    @Test
+    void TtestCitizen() {
+
+
+        boolean boolCitizen = false;
+        int summeRollen = 0;
+        for(int i = 0; i< Controller.getSpielerListe().size(); i++) {
+            if(Controller.getSpielerListe().elementAt(i).getStatus()==true){
+                summeRollen += Controller.getSpielerListe().elementAt(i).getRolle();
+            }}
+        if(summeRollen == 0){
+            boolCitizen = true;
+
+        }
+        else {
+            boolCitizen = false;
+
+        }
+        assertTrue(boolCitizen);
+    }
+
+}
